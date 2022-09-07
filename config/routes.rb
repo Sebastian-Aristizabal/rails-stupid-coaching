4db7aeaeda5b2cbfb921ed282088c6daec23337a
Sebastian Aristizabal <sebastinn@hotmail.com>
2022-09-07 18:24:20 -0500

    finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "ask", to: "questions#ask"
+  # root "articles#index"
+  get "answer", to: "questions#answer"
 end
